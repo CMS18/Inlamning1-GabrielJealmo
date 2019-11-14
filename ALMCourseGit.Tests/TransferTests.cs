2beb4905ad1193ae49bbb4c30af2975aff670067
max-dz <max-dz@hotmail.com>
2019-11-14 12:07:22 +0100

    Added functionality for money transfer between accounts. Added menu item for Transferpage Added unit-tests to check different transfers

---
+++ ALMCourseGit.Tests/TransferTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ALMCourseGit.Web.Database;
+using ALMCourseGit.Web.Database.Entities;
+using Xunit;
+
+namespace ALMCourseGit.Tests
+{
+    public class TransferTests
+    {
+        private readonly Customer Customer = new Customer()
+        {
+            CustomerId = 1,
+            Name = "Test",
+            Accounts = new List<Account>()
+            {
+                new Account()
+                {
+                    AccountId = 1,
+                    Balance = 5000.0M
+                },
+                new Account()
+                {
+                    AccountId = 2,
+                    Balance = 5000.0M
+                }
+            }
+        };
+
+        [Fact]
+        public void SuccessfullTransfer()
+        {
+            var accountSender = Customer.Accounts.First();
+            var accountReceiver = Customer.Accounts.Last();
+            decimal transferAmount = 500M;
+            var expectedBalanceOnSenderAccount = 4500M;
+            var expectedBalanceOnReceiverAccount = 5500M;
+            var expectedMsg = $"Successfully transferred {transferAmount} from account {accountSender.AccountId}. " +
+                  $"Current balance on accounts: {accountSender.AccountId} = {accountSender.Balance}. " +
+                  $"{accountReceiver.AccountId} = {accountReceiver.Balance}";
+
+            BankRepository.AddAccounts(Customer.Accounts);
+
+            var result = BankRepository.Transfer(accountSender.AccountId, accountReceiver.AccountId, transferAmount);
+
+            Assert.Equal(result, expectedMsg);
+            Assert.Equal(expectedBalanceOnSenderAccount, accountSender.Balance);
+            Assert.Equal(expectedBalanceOnReceiverAccount, accountReceiver.Balance);
+        }
+
+        [Fact]
+        public void TransferAmountTooHigh()
+        {
+            var accountSender = Customer.Accounts.First();
+            var accountReceiver = Customer.Accounts.Last();
+            decimal transferAmount = 50000M;
+            var expectedBalanceOnSenderAccount = 5000M;
+            var expectedBalanceOnReceiverAccount = 5000M;
+            var expectedMsg = "You have insufficient founds";
+
+            BankRepository.AddAccounts(Customer.Accounts);
+
+            var result = BankRepository.Transfer(accountSender.AccountId, accountReceiver.AccountId, transferAmount);
+
+            Assert.Equal(result, expectedMsg);
+            Assert.Equal(expectedBalanceOnSenderAccount, accountSender.Balance);
+            Assert.Equal(expectedBalanceOnReceiverAccount, accountReceiver.Balance);
+        }
+    }
+}
---
+++ ALMCourseGit.Web/Controllers/TransferController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ALMCourseGit.Web.Database;
+using ALMCourseGit.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ALMCourseGit.Web.Controllers
+{
+    public class TransferController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(TransferViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            TempData["Status"] = BankRepository.Transfer(model.SenderAccountId, model.ReceiverAccountId, model.Amount);
+
+            return View();
+        }
+    }
+}
